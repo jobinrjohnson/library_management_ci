5870e4542143d51a219197f2a4a9fd7b9c97dda8
Jobin Johnson <jobinrjohnson@gmail.com>
2017-11-13 06:55:28 +0530

    add home status

--- application/views/home.php
+++ application/views/home.php
@@ -13,17 +13,30 @@
     <body>
 
         <div class="container">
-            
+
             <div class="row">
 
                 <?php $this->load->view('include/navbar'); ?>
 
                 <div class="col-md-9">
                     <h2>Welcome Admin</h2>
-                    
-                    
-                    
-                    
+                   
+                    <table width="100%" cellpadding="20px">
+                        <tr>
+                            <td>
+                                <h2><?php echo $status->user_count; ?></h2>
+                                <br>
+                                Users
+                            </td>
+                            <td>
+                                <h2><?php echo $status->book_count; ?></h2>
+                                <br>
+                                Books
+                            </td>
+                        </tr>
+                    </table>
+
+
                 </div>
             </div>
 
